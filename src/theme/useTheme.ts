d088fd06bcdabaa9875b9e70c3ab37a585c8e342
Kazakov Michael <kazakov_my@nlmk.com>
2022-10-15 11:12:57 +0300

    initial commit

---
+++ src/theme/useTheme.ts
@@ -0,0 +1,17 @@
+import {LS_THEME_KEY, Theme, ThemeContext} from "./ThemeContext";
+import {useContext} from "react";
+
+interface UseThemeRes {
+    switchTheme: () => void;
+    theme: Theme;
+}
+
+export function useTheme() {
+    const {theme, setTheme} = useContext(ThemeContext);
+    const switchTheme = () => {
+        const newTheme = theme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT
+        setTheme(newTheme);
+        localStorage.setItem(LS_THEME_KEY, newTheme);
+    }
+    return {theme, switchTheme};
+}
