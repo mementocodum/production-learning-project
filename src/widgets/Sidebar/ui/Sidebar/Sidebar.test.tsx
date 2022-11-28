c47ef9dc7a268af3f307f83c005e1db90419eb61
Kazakov Michael <kazakov_my@nlmk.com>
2022-11-28 21:48:08 +0300

    fix tests

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,8 +1,7 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import { withTranslation } from 'react-i18next';
 import {
     renderWithTranslation,
-} from 'shared/lib/tests/renderWithTranslation/renderWithTranslation';
+} from '../../../../shared/lib/tests/renderWithTranslation/renderWithTranslation';
 import { Sidebar } from './Sidebar';
 
 describe('sidebar test', () => {
